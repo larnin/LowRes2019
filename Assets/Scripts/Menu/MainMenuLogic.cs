dce94faa38e5b77f863ed0622f6a54bca3d2cb63
larnin <nicolas-donald.laurent@laposte.net>
2019-08-13 00:44:49 +0200

    Menus and scene system

---
+++ Assets/Scripts/Menu/MainMenuLogic.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuLogic : MonoBehaviour
+{
+    [SerializeField] string m_sceneName = "";
+
+    SubscriberList m_subscriberList = new SubscriberList();
+
+    private void Awake()
+    {
+        m_subscriberList.Add(new Event<ButtonPressEvent>.Subscriber(OnMenuSelect));
+        m_subscriberList.Subscribe();
+    }
+
+    private void OnDestroy()
+    {
+        m_subscriberList.Unsubscribe();
+    }
+
+    void OnMenuSelect(ButtonPressEvent e)
+    {
+        switch(e.index)
+        {
+            case 0:
+                SceneSystem.changeScene(m_sceneName);
+                break;
+            case 1:
+                SceneSystem.changeScene(m_sceneName);
+                break;
+            case 2:
+                Application.Quit();
+                break;
+            default:
+                Debug.LogError("Help me please, i don't know what to do with that button :'(");
+                break;
+        }
+    }
+}
