49b8e5e9092dd29fac2ad2a7c172f52115a86d79
larnin <nicolas-donald.laurent@laposte.net>
2019-08-15 21:00:45 +0200

    Water and movable output

---
+++ Assets/Scripts/Mechanism/Outputs/MoveObjectOutput.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class MoveObjectOutput : BaseOutput
+{
+    [SerializeField] Vector2 m_enableOffset = Vector2.zero;
+    [SerializeField] float m_moveDuration = 1;
+
+    bool m_active = false;
+    Vector3 m_offPos;
+    Vector3 m_onPos;
+
+    private void Start()
+    {
+        m_offPos = transform.position;
+        m_onPos = transform.position + new Vector3(m_enableOffset.x, m_enableOffset.y, 0);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        var renderers = GetComponentsInChildren<Renderer>();
+        Vector2 min = transform.position;
+        Vector2 max = transform.position;
+
+        foreach(var r in renderers)
+        {
+            var b = r.bounds;
+            var bMin = b.min;
+            var bMax = b.max;
+
+            min.x = Mathf.Min(min.x, bMin.x);
+            min.y = Mathf.Min(min.y, bMin.y);
+            max.x = Mathf.Max(max.x, bMax.x);
+            max.y = Mathf.Max(max.y, bMax.y);
+        }
+        min += m_enableOffset;
+        max += m_enableOffset;
+
+        Gizmos.color = new Color(0, 0, 255);
+        Vector3 pos1 = new Vector3(min.x, min.y, transform.position.z);
+        Vector3 pos2 = new Vector3(max.x, min.y, transform.position.z);
+        Gizmos.DrawLine(pos1, pos2);
+        pos1 = pos2;
+        pos2.y = max.y;
+        Gizmos.DrawLine(pos1, pos2);
+        pos1 = pos2;
+        pos2.x = min.x;
+        Gizmos.DrawLine(pos1, pos2);
+        pos1 = pos2;
+        pos2.y = min.y;
+        Gizmos.DrawLine(pos1, pos2);
+
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(m_enableOffset.x, m_enableOffset.y, 0));
+    }
+
+    public override void SetActiveStatus(bool active)
+    {
+        if (active == m_active)
+            return;
+
+        m_active = active;
+
+        transform.DOMove(m_active ? m_onPos : m_offPos, m_moveDuration);
+    }
+}
---
+++ Assets/Scripts/Mechanism/Outputs/WaterFlowOutput.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+
+public class WaterFlowOutput : BaseOutput
+{
+    [SerializeField] float m_enableFrequency = 1;
+
+    Animator m_animator;
+
+    List<GameObject> m_waterChilds = new List<GameObject>();
+
+    private void Awake()
+    {
+        m_animator = GetComponent<Animator>();
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            m_waterChilds.Add(transform.GetChild(i).gameObject);
+        }
+    }
+
+    public override void SetActiveStatus(bool active)
+    {
+        for (int i = 0; i < m_waterChilds.Count; i++)
+        {
+            int j = i;
+            DOVirtual.DelayedCall((i + 1) * 1 / m_enableFrequency, () => { m_waterChilds[j].SetActive(active); });
+        }
+
+        m_animator.SetBool("Flow", active);
+    }
+}
