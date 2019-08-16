a704eae750be4341510ad8c3e344f23a5128f801
larnin <nicolas-donald.laurent@laposte.net>
2019-08-17 01:57:31 +0200

    Death, insanity, fade, menu loop

---
+++ Assets/Scripts/EventChecker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class EventChecker : MonoBehaviour
+{
+    [SerializeField] float m_deathFadeDelay = 2;
+    [SerializeField] string m_sceneAfterDeath = "";
+
+    SubscriberList m_subscriberList = new SubscriberList();
+
+    private void Awake()
+    {
+        m_subscriberList.Add(new Event<PlayerDeathEvent>.Subscriber(OnDeath));
+        m_subscriberList.Subscribe();
+    }
+
+    private void OnDestroy()
+    {
+        m_subscriberList.Unsubscribe();
+    }
+
+    void OnDeath(PlayerDeathEvent e)
+    {
+        DOVirtual.DelayedCall(m_deathFadeDelay, () =>
+        {
+            if (this == null)
+                return;
+
+            SceneSystem.changeScene(m_sceneAfterDeath);
+        });
+    }
+}
